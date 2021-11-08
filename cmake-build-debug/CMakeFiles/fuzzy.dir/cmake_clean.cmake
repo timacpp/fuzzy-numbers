46581a062e4089e4d508e909b5084f47ff9e1818
Tima <timathevedmed@gmail.com>
2021-11-08 14:11:53 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/fuzzy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fuzzy.dir/main.cpp.o"
+  "fuzzy"
+  "fuzzy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fuzzy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
